eb0918703300d1fbd1559edaf57087891568da79
Pablo1009010 <pablo1009010@gmail.com>
2026-09-13 19:44:48 -0600

    feat: agrega removeProduct al ProductProvider conectado a DeleteProductUseCase

--- lib/presentation/providers/product_provider.dart
+++ lib/presentation/providers/product_provider.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 import '../../domain/usecases/add_product_usecase.dart';
+import '../../domain/usecases/delete_product_usecase.dart';
 
 class ProductProvider extends ChangeNotifier {
   final AddProductUseCase addProductUseCase;
+  final DeleteProductUseCase deleteProductUseCase;
 
-  ProductProvider({required this.addProductUseCase});
+  ProductProvider({
+    required this.addProductUseCase,
+    required this.deleteProductUseCase,
+  });
 
   bool _isLoading = false;
   bool get isLoading => _isLoading;
@@ -40,6 +45,24 @@ class ProductProvider extends ChangeNotifier {
         category: category,
       );
 
+      _isLoading = false;
+      notifyListeners();
+      return true;
+    } catch (e) {
+      _errorMessage = e.toString();
+      _isLoading = false;
+      notifyListeners();
+      return false;
+    }
+  }
+
+  Future<bool> removeProduct(int id) async {
+    _isLoading = true;
+    _errorMessage = null;
+    notifyListeners();
+
+    try {
+      await deleteProductUseCase.execute(id);
       _isLoading = false;
       notifyListeners();
       return true;
